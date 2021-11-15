0724294b70705335af4ba43b0f0bf7d51464f5de
Tuhin117x <92184005+Tuhin117x@users.noreply.github.com>
2021-11-15 17:53:49 +0530

    Add files via upload

---
+++ 1. Pre-Processing/pre_process_train.py
@@ -0,0 +1,112 @@
+import json
+import pandas as pd
+import numpy as np
+import os
+
+def wrangledata(df,name):
+	df2=df['textAnnotations'].apply(pd.Series)
+	#print(df2)
+	dataframe1=pd.DataFrame(df2['description'])
+	#return dataframe1
+	df3=df2['boundingPoly'].apply(pd.Series)
+	#print(df3)
+	df4=df3['vertices'].apply(pd.Series)
+	#print(df4)
+
+	df5=df4[0].apply(pd.Series)
+	dataframe2=pd.DataFrame(df5)
+	dataframe2.columns=['x0','y0']
+	#print(dataframe2)
+
+	df6=df4[1].apply(pd.Series)
+	dataframe3=pd.DataFrame(df6)
+	dataframe3.columns=['x1','y1']
+	#print(dataframe3)
+
+	df7=df4[2].apply(pd.Series)
+	dataframe4=pd.DataFrame(df7)
+	dataframe4.columns=['x2','y2']
+	#print(dataframe4)
+
+	df8=df4[3].apply(pd.Series)
+	dataframe5=pd.DataFrame(df8)
+	dataframe5.columns=['x3','y3']
+	#print(dataframe5)
+
+
+	final_df=pd.concat([dataframe1,dataframe2,dataframe3,dataframe4,dataframe5],axis=1)
+	#print(final_df)
+
+	final_df['id']=np.arange(len(final_df))
+	final_df['imagename']=name.split('.',1)[0]
+	#print(final_df)
+	return final_df
+
+
+def wrangledata2(df,name):
+	print("a")
+	name2=name.split('.',1)[0]
+	name3=name2.split('_',1)[0]+'_'+name2.split('_')[1]
+	#df['imagename']=name.split('.',1)[0]
+	df['imagename']=name3
+	#df['imagename']=name.split('_',1)[0]+name.split('_',1)[1]
+	dfk=df[['label','id','imagename','text']]
+	return dfk
+
+path="E:\Storage Access\Workspaces\AMEX\TextCNN\Datasets\Train\input"
+os.chdir(path)
+
+basedf=pd.DataFrame(columns=['description','x0','y0','x1','y1','x2','y2','x3','y3','id','imagename'])
+#print(basedf)
+
+
+for file in os.listdir():
+	if file.endswith(".json"):
+		file_path=f"{path}\{file}"
+		name=f"{file}"
+		print(file_path)
+		print(name)
+		with open(name) as f:
+			#id0=json.load(f)
+			df=pd.read_json(name)
+			#print(df)
+			displaydf=wrangledata(df,name)
+			#print(displaydf)
+			basedf=pd.concat([basedf,displaydf],axis=0)
+			#print(basedf)
+
+
+
+#print(basedf)
+#basedf.to_csv('train.csv',index=False)			
+
+path2="E:\Storage Access\Workspaces\AMEX\TextCNN\Datasets\Train\Output"
+os.chdir(path2)
+#print(os.listdir())
+
+
+basedf2=pd.DataFrame(columns=['label','id','imagename'])
+
+for file in os.listdir():
+	if file.endswith(".json"):
+		file_path=f"{path}\{file}"
+		name=f"{file}"
+		print(file_path)
+		print(name)
+		with open(name) as f:
+			#id0=json.load(f)
+			df=pd.read_json(name)
+			#print(df)
+			displaydf=wrangledata2(df,name)
+			#print(displaydf)
+			basedf2=pd.concat([basedf2,displaydf],axis=0)
+			#print(basedf)
+
+#print(basedf2)
+#basedf2.to_csv('train_labels.csv',index=False)
+
+final_df=basedf.merge(basedf2,on=['imagename','id'],how='left')
+path3="E:\Storage Access\Workspaces\AMEX\TextCNN"
+os.chdir(path3)
+
+final_df.to_csv('train.csv',index=False)
